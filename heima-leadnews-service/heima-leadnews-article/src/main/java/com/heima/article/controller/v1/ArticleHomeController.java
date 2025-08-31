2bf4b56315e943e6b0af9076f4e063bd829ff419
yangdi <3646942424@qq.com>
2025-08-31 11:40:30 +0800

    feat: 把article推荐显示出来了，目前没发现别的问题，准备学freemarker

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
@@ -0,0 +1,31 @@
+package com.heima.article.controller.v1;
+
+import com.heima.article.service.ApArticleService;
+import com.heima.common.constants.ArticleConstants;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/article")
+public class ArticleHomeController {
+    @Autowired
+    private ApArticleService apArticleService;
+    @RequestMapping("/load")  //加载首页
+    public ResponseResult load(@RequestBody ArticleHomeDto dto){
+        return apArticleService.load(dto, ArticleConstants.LOADTYPE_LOAD_MORE);
+    }
+
+    @RequestMapping("/loadmore")  //加载更多
+    public ResponseResult loadmore(@RequestBody ArticleHomeDto dto){
+        return apArticleService.load(dto, ArticleConstants.LOADTYPE_LOAD_MORE);
+    }
+
+    @RequestMapping("/loadnew")  //加载最新
+    public ResponseResult loadnew(@RequestBody ArticleHomeDto dto){
+        return apArticleService.load(dto, ArticleConstants.LOADTYPE_LOAD_NEW);
+    }
+}
